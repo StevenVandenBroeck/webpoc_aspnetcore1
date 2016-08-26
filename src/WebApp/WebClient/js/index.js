38cd88a62f2a36126fd2f772487b9f7fec183f7c
Steven Vanden Broeck <xytyx@telenet.be>
2016-08-26 09:01:27 +0200

    Webpack with jQuery and Bootstrap Sass

--- src/WebApp/WebClient/js/index.js
+++ src/WebApp/WebClient/js/index.js
@@ -1,2 +1,7 @@
+var $ = require('jquery');
 var text = require('./text.js');
-document.getElementById("webpack-content").innerHTML = text;
+
+//document.getElementById("webpack-content").innerHTML = text;
+$('#webpack-content').html(text + ' with jQuery');
+
+console.log("index loaded");
--- src/WebApp/webpack.config.js
+++ src/WebApp/webpack.config.js
@@ -1,10 +1,28 @@
 var path = require('path');
+var webpack = require('webpack');
+
+var webClientPath = path.join(__dirname, 'WebClient');
 
 module.exports = {
      context: path.join(__dirname, 'WebClient/js'),
-     entry: './index.js',
+     entry: ['./index.js'],
      output: {
           path: path.join(__dirname, 'wwwroot/js'),
           filename: 'index.bundle.js'
-     }
+     },
+     module:{
+         loaders: [
+             { test: /\.css$/, loaders: ['style', 'css'] }, 
+             { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }, 
+             { test: /\.(woff2?|svg)$/, loader: 'url?limit=10000' },
+             { test: /\.(ttf|eot)$/, loader: 'file' },
+             { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
+         ]
+     },
+     plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
